f69d9471709d74619b61b452f8c113fb82f40ff3
Matěj Týč <matej.tyc@gmail.com>
2019-09-08 16:57:53 +0200

    Updated the tests documentation.

--- docker/exec_test.sh
+++ docker/exec_test.sh
@@ -1,1 +1,4 @@
+# This script is optimized for simplicity and speed.
+# It assumes that the openldap container runs when the test script is executed.
+# Then, it executes the test container, and scraps and re-creates openldap, so it is ready for the next test run.
 docker-compose up python && docker-compose rm -fs openldap && docker-compose up -d openldap
